e0102ec253b3fb31bcd4ac5076c581018eea784c
red_angel <zhe.73@bk.ru>
2021-10-03 09:15:55 +0400

    Added actions to test () for Aliexpress.ru

--- src/main/java/core/WebDriver.kt
+++ src/main/java/core/WebDriver.kt
@@ -2,13 +2,18 @@ package testSuite
 
 import consts.Constants.CHROMEDRIVER_BIN_PATH
 import consts.Constants.CHROMEDRIVER_EXE_PATH
-import org.openqa.selenium.WebDriver
+import org.openqa.selenium.*
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 import props.Config
+import java.io.File
+import java.util.*
 
 
 object Driver {
     val driver: WebDriver
+    var wait: WebDriverWait
 
     init {
         val driverPath = when (Config.operatingSystem) {
@@ -18,6 +23,36 @@ object Driver {
         }
         System.setProperty("webdriver.chrome.driver", driverPath)
         driver = ChromeDriver()
+        wait = WebDriverWait(driver, 10)
+    }
+
+    fun getElements(locator: By?): List<WebElement>? {
+        return driver.findElements(locator)
+    }
+
+    fun click(aNew: By?) {
+        driver.findElement(aNew).click()
+    }
+
+    fun type(locator: By?, name: String?) {
+        if (name != null) {
+            driver.findElement(locator).clear()
+            driver.findElement(locator).sendKeys(name)
+        }
+    }
+
+    fun pressKey(locator: By?, key: Keys) {
+        driver.findElement(locator).sendKeys(key)
+    }
+
+    fun takeScreenshot() {
+        (this as TakesScreenshot)
+                .getScreenshotAs(OutputType.FILE)
+                .copyTo(File("screenshots/${UUID.randomUUID()}.jpg"))
+    }
+
+    fun waitForElement(locator: By?) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator))
     }
 
 }
--- src/test/java/testSuite/PuppeterTest.kt
+++ src/test/java/testSuite/PuppeterTest.kt
@@ -1,13 +1,38 @@
 package testSuite
 
 import org.junit.jupiter.api.Test
+import org.openqa.selenium.By
+import org.openqa.selenium.Keys
 import props.Config.baseUrl
+import testSuite.Driver.click
+import testSuite.Driver.getElements
+import testSuite.Driver.pressKey
+import testSuite.Driver.takeScreenshot
+import testSuite.Driver.type
+import testSuite.Driver.waitForElement
+
 
 class PuppeterTest {
     @Test
     fun test() {
         Driver.driver.apply {
             this.get(baseUrl)
+            try {
+                click(By.xpath("//*[contains(@class,'CouponPopup_CouponPopup__closeButton')]"))
+                click(By.className("Header_SearchInput__combobox__85mzj"))
+                type(By.xpath("//*[contains(@class,'Header_SearchInput__input')]"), "nylon shoulder bag")
+                pressKey(By.xpath("//*[contains(@class,'Header_SearchInput__input')]"), Keys.RETURN)
+                click(By.xpath("//*[contains(@class,'next-dialog-close')]"))
+                //TODO
+                waitForElement(By.xpath("//*[contains(@class,'next-checkbox-input')]"))
+                getElements(By.xpath("//*[contains(@class,'next-checkbox-input')]"))?.first()?.click()
+                click(By.xpath("//*[@product-index='3']"))
+                takeScreenshot()
+
+            } finally {
+                this.quit()
+            }
+
         }
     }
 
